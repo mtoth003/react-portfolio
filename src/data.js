ad06eb6cc5c3e1727c781c5bbcd10c22c2db5d04
Michael Toth <mtoth003@gmail.com>
2022-02-19 23:43:25 -0500

    pushing portfolio, new repo

---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    title: "FlatIron Hub",
+    subtitle: "React and Firebase",
+    description:
+      "React application for Flatiron students to use. Uses React with Hooks and Context for state management. The application has 4 main components. The Chat uses ChatEngine as a rest API to create a chat room. The Games and Learn components, both use external or self hosted APIs. The VideoChat component using PeerJs a WebRTC wrapper to create a peer to peer connection between two users.",
+    image: "./flatiron.png",
+    link: "https://flatironhub.org/",
+  },
+  {
+    title: "RotoRoundup",
+    subtitle: "React and Ruby on Rails",
+    description:
+      "An application built with a React frontend, Ruby on Rails backend API and a PostgreSQL database. This app allows users to create and account and sign in to the site, sessions are stores in cookies. Once signed in to the app, users can review Fantasy Sports sites, or create a forum thread to discuss any topic. Authorized site admins can delete users and forum threads. This app is deployed with Heroku and may take a minute to start.",
+    image: "./rotoroundup.png",
+    link: "https://roto-roundup.herokuapp.com/",
+  },
+  {
+    title: "devsConnect",
+    subtitle: "React and Ruby on Rails",
+    description:
+      "This application was built using a React frontend with a Ruby on Rails backend. The purpose of dev*Connect* is to serve as a platform for web developers to share significant and useful programming content from blogs to tutorials to cool YouTube demos. From 'My Feed', the user can view posts from all others users with the ability to filter by desired topics. From 'My Page', the user can view all of their own posts, update their account information such as Github and LinkedIn, or create a new post.",
+    image: "./devsconnect.png",
+    link: "https://devsconnect-app.herokuapp.com/",
+  },
+  {
+    title: "Subscription Tracker",
+    subtitle: "React and Firebase",
+    description:
+      "A simple subscription tracker that allows a user to add track their current subscriptions. Data is stored in a Google Firebase Database to for persistance. Users are able to add, edit and delete subscriptions. The site is deployed with Firebase as well.",
+    image: "./subscriptiontracker.png",
+    link: "https://subscription-tracker-9fc70.web.app/",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "HTML",
+  "CSS",
+  "Ruby",
+  "Rails",
+  "PostgreSQL",
+  "ActiveRecord",
+];
